ef69fc754973659fc9c4376c9453b01421fb9482
Catherine Noll <noll.catherine@gmail.com>
2022-09-07 15:00:19 -0400

    Print custom config value to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    match env::current_dir() {
+        Ok(dir) => {
+            match git2::Repository::discover(dir) {
+                Ok(repo) => match repo.config() {
+                    Ok(c) => match c.get_entry("myconfigsection.custom") {
+                        Ok(entry) => match entry.value() {
+                            Some(value) => println!("{}", value),
+                            None => println!("No value XXXX"),
+                        },
+                        Err(err) => {
+                            println!("Error: {}", err)
+                        }
+                    },
+                    Err(err) => {
+                        println!("Error: {}", err)
+                    }
+                },
+                Err(err) => {
+                    // git2::Config::open_default().ok()
+                    println!("Error: {}", err)
+                }
+            };
+        }
+        Err(err) => {
+            println!("Error: {}", err)
+        }
+    };
 }
